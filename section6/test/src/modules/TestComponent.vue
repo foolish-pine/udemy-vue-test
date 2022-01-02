c5afe40dc837e71e43aa577aa365d5acde76927e
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-01-02 11:31:36 +0900

    section6

---
+++ section6/test/src/modules/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="red-b">テストです<br />{{ testData }}</div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      testData: "テストデータです",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.red-b {
+  border: 1px solid red;
+}
+</style>
